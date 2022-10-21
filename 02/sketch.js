042f5799e5f7d4af5433d3e4747f35f23de81b8a
mathiasliniger <mathiasliniger@gmail.com>
2022-10-21 08:43:33 +0200

    02remake

--- 02/sketch.js
+++ 02/sketch.js
@@ -10,27 +10,23 @@ function setup() {
 }
 
 function draw() {
-  let c = cos(0);
+
 
   fill(255);
   
   
-  //increase by a little bit each frame.
+ 
   angle += 0.05; 
   
   
-  //https://static.scientificamerican.com/blogs/assets/File/Circle_cos_sin.gif
+ 
   ypos = sin(angle) * scalar + height/2;
   xpos = cos(angle) * scalar + width/2;
-  
-  //spiraling paths if you animate the scalar number!
+
   scalar += 0.1
   
-  
-  // Mueve la figura al centro del lienzo
-  translate(width / 2, height / 2);
-  // Aplica la rotación final
-  rotate(c);
+
+
 
   ellipse(xpos, ypos, 10,100)
 }
